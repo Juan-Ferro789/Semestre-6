3839416e4d384c715642a445932a51a6ff5aa91d
Juan Ferro <jcferro@ucompensar.edu.co>
2025-03-20 20:05:14 -0500

    Pokemon pokedex

---
+++ src/components/HorizontalBar.jsx
@@ -0,0 +1,9 @@
+function HorizontalBar(){
+    return(
+        <div className="horizontal-bar">
+            <img src="/public/img/Pokemon.jpg" alt="Pokemon Logo" className="Pokemon-logo"/>
+        </div>
+    )
+}
+
+export default HorizontalBar;
